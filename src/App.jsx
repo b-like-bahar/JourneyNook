91170a64ffee4b9b98b5d18c33a03d698393d807
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-14 18:28:50 -0600

    import necessary pages and components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import './App.scss';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Header from "./components/Header/Header";
-
+import Header from "./components/Header/Header.jsx";
+import Footer from './components/Footer/Footer.jsx';
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import CityDetailPage from "./pages/CityDetailPage/CityDetailPage.jsx";
+import ItineraryPage from "./pages/ItineraryPage/ItineraryPage.jsx";
+import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 function App() {
 
   return (
     <>
     <BrowserRouter>
+    <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/city/:id" element={<CityDetailPage />} />
@@ -14,6 +19,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
+        <Footer />
     </BrowserRouter>
     </>
   )
