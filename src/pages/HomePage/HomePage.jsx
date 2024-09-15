d6ec88770c00d0af46c25569485cc6fb0739eeb6
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-15 04:38:21 -0600

    add hero section with its style to the home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,19 @@ import "./HomePage.scss"
 
 function HomePage() {
     return (
-        <div>HomePage</div>
+        <div className="hero">
+            <div className="hero__quote">
+            <h1 className="hero__quote-firstLine">
+                “Traveling – it leaves you speechless,
+            </h1>
+            <h1 className="hero__quote-secondLine">
+                then turns you into a storyteller.”
+            </h1>
+            <h3 className="hero__quote-name">
+                – Ibn Battuta
+            </h3>
+            </div>
+        </div>
     )
 }
 
