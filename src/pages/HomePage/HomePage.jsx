7d4d8d070e7d92158c30a14681bb04cadf328c50
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-15 05:04:38 -0600

    add about section to the hero section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,19 +2,29 @@ import "./HomePage.scss"
 
 function HomePage() {
     return (
-        <div className="hero">
-            <div className="hero__quote">
-            <h1 className="hero__quote-firstLine">
-                “Traveling – it leaves you speechless,
-            </h1>
-            <h1 className="hero__quote-secondLine">
-                then turns you into a storyteller.”
-            </h1>
-            <h3 className="hero__quote-name">
-                – Ibn Battuta
-            </h3>
+        <>
+            <div className="hero">
+            <div className="hero__item">
+                <div className="hero__item-quote">
+                    <h1 className="hero__item-quote-firstLine">
+                        “Traveling – it leaves you speechless,
+                    </h1>
+                    <h1 className="hero__item-quote-secondLine">
+                        then turns you into a storyteller.”
+                    </h1>
+                    <h3 className="hero__item-quote-name">
+                        – Ibn Battuta
+                    </h3>
+                </div>
             </div>
-        </div>
+            <p className="hero__about">
+                At JourneyNook, our purpose is to inspire and guide travelers through every step of their journey. 
+                Whether you’re dreaming of your next destination or planning every detail of your trip, 
+                JourneyNook is here to make your travel experience seamless and enjoyable.
+            </p>
+            </div>
+        </>
+
     )
 }
 
