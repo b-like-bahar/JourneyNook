d7564482bfa57d32aca492a329fc3973927a9916
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-16 11:07:53 -0600

    updated images folder, updated homapage.jsx and its style to have new cities list

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,15 @@
 import "./HomePage.scss"
-import parisImage from '../../assets/images/paris-France.jpg';
+import amsterdamImage from '../../assets/images/Amsterdam-Netherlands.png';
+import barcelonaImage from '../../assets/images/Barcelona-Spain.png';
+import istanbulImage from '../../assets/images/Istanbul-Turkey.png';
+import londonImage from '../../assets/images/London-England.png';
+import moscowImage from '../../assets/images/Moscow-Russia.png';
+import parisImage from '../../assets/images/Paris-France.png';
+import rioImage from '../../assets/images/Rio-Brasil.png';
+import romeImage from '../../assets/images/Rome-Italy.png';
+import shanghaiImage from '../../assets/images/Shanghai-China.png';
+import sydneyImage from '../../assets/images/Sydney-Australia.png';
+
 function HomePage() {
     return (
         <>
@@ -25,6 +35,73 @@ function HomePage() {
                         JourneyNook is here to make your travel experience seamless and enjoyable.
                     </p>
                 </div>
+            </div>
+            <div className="destinations">
+                <h2 className="destinations__title">
+                Explore top spots for your next adventure
+                </h2>
+                <ul className="destinations__pics">
+                    <li className="destinations__pics-item">
+                        <img className="destinations__pics-item-img" src={ amsterdamImage } />
+                        <span className="destinations__pics-item-location">
+                        Amsterdam, Netherlands
+                        </span>
+                    </li>
+                    <li className="destinations__pics-item">
+                        <img className="destinations__pics-item-img" src={ barcelonaImage } />
+                        <span className="destinations__pics-item-location">
+                        Barcelona, Spain
+                        </span>
+                    </li>
+                    <li className="destinations__pics-item">
+                        <img className="destinations__pics-item-img" src={ istanbulImage } />
+                        <span className="destinations__pics-item-location">
+                            Istanbul, Turkey
+                        </span>
+                    </li>
+                    <li className="destinations__pics-item">
+                        <img className="destinations__pics-item-img" src={ londonImage } />
+                        <span className="destinations__pics-item-location">
+                            London, England
+                        </span>
+                    </li>
+                    <li className="destinations__pics-item">
+                        <img className="destinations__pics-item-img" src={ moscowImage } />
+                        <span className="destinations__pics-item-location">
+                            Moscow, Russia
+                        </span>
+                    </li>
+                    <li className="destinations__pics-item">
+                        <img className="destinations__pics-item-img" src={ parisImage } />
+                        <span className="destinations__pics-item-location">
+                            Paris, France
+                        </span>
+                    </li>
+                    <li className="destinations__pics-item">
+                        <img className="destinations__pics-item-img" src={ rioImage } />
+                        <span className="destinations__pics-item-location">
+                            Rio, Brasil
+                        </span>
+                    </li>
+                    <li className="destinations__pics-item">
+                        <img className="destinations__pics-item-img" src={ romeImage } />
+                        <span className="destinations__pics-item-location">
+                            Rome, Italy
+                        </span>
+                    </li>
+                    <li className="destinations__pics-item">
+                        <img className="destinations__pics-item-img" src={ shanghaiImage } />
+                        <span className="destinations__pics-item-location">
+                            Shanghai, China
+                        </span>
+                    </li>
+                    <li className="destinations__pics-item">
+                        <img className="destinations__pics-item-img" src={ sydneyImage } />
+                        <span className="destinations__pics-item-location">
+                            Sydney, Australia
+                        </span>
+                    </li>
+                </ul>
             </div>
         </>
 
