9ae70e9589e3ab505e3faccaf309e631a6f188b7
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-15 21:04:35 -0600

    git commit update home page to have aboute sectio and style it

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,30 @@
 import "./HomePage.scss"
-
+import parisImage from '../../assets/images/paris-France.jpg';
 function HomePage() {
     return (
         <>
             <div className="hero">
-            <div className="hero__item">
-                <div className="hero__item-quote">
-                    <h1 className="hero__item-quote-firstLine">
-                        “Traveling – it leaves you speechless,
-                    </h1>
-                    <h1 className="hero__item-quote-secondLine">
-                        then turns you into a storyteller.”
-                    </h1>
-                    <h3 className="hero__item-quote-name">
-                        – Ibn Battuta
-                    </h3>
+                <div className="hero__item">
+                    <div className="hero__item-quote">
+                        <h1 className="hero__item-quote-firstLine">
+                            “Traveling – it leaves you speechless,
+                        </h1>
+                        <h1 className="hero__item-quote-secondLine">
+                            then turns you into a storyteller.”
+                        </h1>
+                        <h3 className="hero__item-quote-name">
+                            – Ibn Battuta
+                        </h3>
+                    </div>
                 </div>
-            </div>
-            <p className="hero__about">
-                At JourneyNook, our purpose is to inspire and guide travelers through every step of their journey. 
-                Whether you’re dreaming of your next destination or planning every detail of your trip, 
-                JourneyNook is here to make your travel experience seamless and enjoyable.
-            </p>
+                <div className="hero__about">
+                    <h2 className="hero__about-title">ABOUT</h2>
+                    <p className="hero__about-text  large">
+                        At JourneyNook, our purpose is to inspire and guide travelers through every step of their journey.
+                        Whether you’re dreaming of your next destination or planning every detail of your trip,
+                        JourneyNook is here to make your travel experience seamless and enjoyable.
+                    </p>
+                </div>
             </div>
         </>
 
