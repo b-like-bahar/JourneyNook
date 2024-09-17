23c6a1919a08444f89c66782afd713395cd5c56d
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-17 02:14:24 -0600

    Add top destinations section and its scroll functionality to homepage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import "./HomePage.scss"
-import amsterdamImage from '../../assets/images/Amsterdam-Netherlands.png';
+import cancunImage from '../../assets/images/Cancun-Mexico.png';
 import barcelonaImage from '../../assets/images/Barcelona-Spain.png';
 import istanbulImage from '../../assets/images/Istanbul-Turkey.png';
 import londonImage from '../../assets/images/London-England.png';
@@ -9,8 +9,58 @@ import rioImage from '../../assets/images/Rio-Brasil.png';
 import romeImage from '../../assets/images/Rome-Italy.png';
 import shanghaiImage from '../../assets/images/Shanghai-China.png';
 import sydneyImage from '../../assets/images/Sydney-Australia.png';
+import scrollArrowIcon from '../../assets/icons/scroll-arrow.svg';
+import { useRef, useState, useEffect } from 'react';
+import { ReactSVG } from "react-svg";
+
 
 function HomePage() {
+    const destinationListRef = useRef(null);
+    const [showLeftButton, setShowLeftButton] = useState(false);
+    const [showRightButton, setShowRightButton] = useState(true);
+
+    const scrollLeft = () => {
+        if (destinationListRef.current) {
+            destinationListRef.current.scrollBy({
+                top: 0,
+                left: -300,
+                behavior: 'smooth'
+            });
+        }
+    };
+
+    const scrollRight = () => {
+        if (destinationListRef.current) {
+            destinationListRef.current.scrollBy({
+                top: 0,
+                left: 300,
+                behavior: 'smooth'
+            });
+        }
+    };
+
+    const checkScrollPosition = () => {
+        if (destinationListRef.current) {
+            const scrollLeftValue = destinationListRef.current.scrollLeft;
+            const maxScrollLeft = destinationListRef.current.scrollWidth - destinationListRef.current.clientWidth;
+
+            setShowLeftButton(scrollLeftValue > 0);
+            setShowRightButton(scrollLeftValue < maxScrollLeft);
+        }
+    };
+
+    useEffect(() => {
+        const listRef = destinationListRef.current;
+        if (listRef) {
+            listRef.addEventListener('scroll', checkScrollPosition);
+        }
+        return () => {
+            if (listRef) {
+                listRef.removeEventListener('scroll', checkScrollPosition);
+            }
+        };
+    }, []);
+
     return (
         <>
             <div className="hero">
@@ -38,70 +88,82 @@ function HomePage() {
             </div>
             <div className="destinations">
                 <h2 className="destinations__title">
-                Explore top spots for your next adventure
+                    Explore top spots for your next adventure
                 </h2>
-                <ul className="destinations__pics">
-                    <li className="destinations__pics-item">
-                        <img className="destinations__pics-item-img" src={ amsterdamImage } />
-                        <span className="destinations__pics-item-location">
-                        Amsterdam, Netherlands
-                        </span>
-                    </li>
-                    <li className="destinations__pics-item">
-                        <img className="destinations__pics-item-img" src={ barcelonaImage } />
-                        <span className="destinations__pics-item-location">
-                        Barcelona, Spain
-                        </span>
-                    </li>
-                    <li className="destinations__pics-item">
-                        <img className="destinations__pics-item-img" src={ istanbulImage } />
-                        <span className="destinations__pics-item-location">
-                            Istanbul, Turkey
-                        </span>
-                    </li>
-                    <li className="destinations__pics-item">
-                        <img className="destinations__pics-item-img" src={ londonImage } />
-                        <span className="destinations__pics-item-location">
-                            London, England
-                        </span>
-                    </li>
-                    <li className="destinations__pics-item">
-                        <img className="destinations__pics-item-img" src={ moscowImage } />
-                        <span className="destinations__pics-item-location">
-                            Moscow, Russia
-                        </span>
-                    </li>
-                    <li className="destinations__pics-item">
-                        <img className="destinations__pics-item-img" src={ parisImage } />
-                        <span className="destinations__pics-item-location">
-                            Paris, France
-                        </span>
-                    </li>
-                    <li className="destinations__pics-item">
-                        <img className="destinations__pics-item-img" src={ rioImage } />
-                        <span className="destinations__pics-item-location">
-                            Rio, Brasil
-                        </span>
-                    </li>
-                    <li className="destinations__pics-item">
-                        <img className="destinations__pics-item-img" src={ romeImage } />
-                        <span className="destinations__pics-item-location">
-                            Rome, Italy
-                        </span>
-                    </li>
-                    <li className="destinations__pics-item">
-                        <img className="destinations__pics-item-img" src={ shanghaiImage } />
-                        <span className="destinations__pics-item-location">
-                            Shanghai, China
-                        </span>
-                    </li>
-                    <li className="destinations__pics-item">
-                        <img className="destinations__pics-item-img" src={ sydneyImage } />
-                        <span className="destinations__pics-item-location">
-                            Sydney, Australia
-                        </span>
-                    </li>
-                </ul>
+                <div className="destinations__scroll">
+                    {showLeftButton && (
+                        <button className="destinations__scroll-button-left" onClick={scrollLeft}>
+                            <ReactSVG className="destinations__scroll-button-left-svg"src={ scrollArrowIcon } />
+                        </button>
+                    )}
+                    <ul className="destinations__pics" ref={destinationListRef}>
+                        <li className="destinations__pics-item">
+                            <img className="destinations__pics-item-img" src={cancunImage} />
+                            <h2 className="destinations__pics-item-location">
+                                Cancun, Mexico
+                            </h2>
+                        </li>
+                        <li className="destinations__pics-item">
+                            <img className="destinations__pics-item-img" src={barcelonaImage} />
+                            <h2 className="destinations__pics-item-location">
+                                Barcelona, Spain
+                            </h2>
+                        </li>
+                        <li className="destinations__pics-item">
+                            <img className="destinations__pics-item-img" src={istanbulImage} />
+                            <h2 className="destinations__pics-item-location">
+                                Istanbul, Turkey
+                            </h2>
+                        </li>
+                        <li className="destinations__pics-item">
+                            <img className="destinations__pics-item-img" src={londonImage} />
+                            <h2 className="destinations__pics-item-location">
+                                London, England
+                            </h2>
+                        </li>
+                        <li className="destinations__pics-item">
+                            <img className="destinations__pics-item-img" src={moscowImage} />
+                            <h2 className="destinations__pics-item-location">
+                                Moscow, Russia
+                            </h2>
+                        </li>
+                        <li className="destinations__pics-item">
+                            <img className="destinations__pics-item-img" src={parisImage} />
+                            <h2 className="destinations__pics-item-location">
+                                Paris, France
+                            </h2>
+                        </li>
+                        <li className="destinations__pics-item">
+                            <img className="destinations__pics-item-img" src={rioImage} />
+                            <h2 className="destinations__pics-item-location">
+                                Rio, Brasil
+                            </h2>
+                        </li>
+                        <li className="destinations__pics-item">
+                            <img className="destinations__pics-item-img" src={romeImage} />
+                            <h2 className="destinations__pics-item-location">
+                                Rome, Italy
+                            </h2>
+                        </li>
+                        <li className="destinations__pics-item">
+                            <img className="destinations__pics-item-img" src={shanghaiImage} />
+                            <h2 className="destinations__pics-item-location">
+                                Shanghai, China
+                            </h2>
+                        </li>
+                        <li className="destinations__pics-item">
+                            <img className="destinations__pics-item-img" src={sydneyImage} />
+                            <h2 className="destinations__pics-item-location">
+                                Sydney, Australia
+                            </h2>
+                        </li>
+                    </ul>
+                    {showRightButton && (
+                        <button className="destinations__scroll-button-right" onClick={scrollRight}>
+                            <ReactSVG className="destinations__scroll-button-right-svg"src={ scrollArrowIcon } />
+                        </button>
+                    )}
+                </div>
             </div>
         </>
 
