f50563489997ca871c572676dc5b1e8658e5b01c
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-17 19:15:28 -0600

    import new image and add a new paragraph to homepage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,6 +10,7 @@ import romeImage from '../../assets/images/Rome-Italy.png';
 import shanghaiImage from '../../assets/images/Shanghai-China.png';
 import sydneyImage from '../../assets/images/Sydney-Australia.png';
 import scrollArrowIcon from '../../assets/icons/scroll-arrow.svg';
+import aboutImage from '../../assets/images/about-us-pic.jpg';
 import { useRef, useState, useEffect } from 'react';
 import { ReactSVG } from "react-svg";
 
@@ -77,13 +78,27 @@ function HomePage() {
                         </h3>
                     </div>
                 </div>
-                <div className="hero__about">
-                    <h2 className="hero__about-title">ABOUT</h2>
-                    <p className="hero__about-text  large">
-                        At JourneyNook, our purpose is to inspire and guide travelers through every step of their journey.
-                        Whether you’re dreaming of your next destination or planning every detail of your trip,
-                        JourneyNook is here to make your travel experience seamless and enjoyable.
-                    </p>
+            </div>
+
+            <div className="about">
+                <h2 className="about__title">About JourneyNook</h2>
+                <div className="about__item">
+                    <div className="about__item-visual">
+                        <img className="about__item-visual-img" src={aboutImage} />
+                        <div className="about__item-visual-border"></div>
+                    </div>
+                    <div className="about__item-description">
+                        <p className="about__item-description-text1  large">
+                            At JourneyNook, our purpose is to inspire and guide travelers through every step of their journey.
+                            Whether you’re dreaming of your next destination or planning every detail of your trip,
+                            JourneyNook is here to make your travel experience seamless and enjoyable.
+                        </p>
+                        <p className="about__item-description-text2  large">
+                        We curate must-see landmarks, hidden gems, and essential travel tips for cities worldwide. 
+                        With our itinerary builder, you can plan your days, adjust activities, and tailor your schedule to your preferences. 
+                        JourneyNook ensures you're prepared to explore confidently, from iconic sights to local hidden gems.
+                        </p>
+                    </div>
                 </div>
             </div>
             <div className="destinations">
@@ -93,7 +108,7 @@ function HomePage() {
                 <div className="destinations__scroll">
                     {showLeftButton && (
                         <button className="destinations__scroll-button-left" onClick={scrollLeft}>
-                            <ReactSVG className="destinations__scroll-button-left-svg"src={ scrollArrowIcon } />
+                            <ReactSVG className="destinations__scroll-button-left-svg" src={scrollArrowIcon} />
                         </button>
                     )}
                     <ul className="destinations__pics" ref={destinationListRef}>
@@ -160,7 +175,7 @@ function HomePage() {
                     </ul>
                     {showRightButton && (
                         <button className="destinations__scroll-button-right" onClick={scrollRight}>
-                            <ReactSVG className="destinations__scroll-button-right-svg"src={ scrollArrowIcon } />
+                            <ReactSVG className="destinations__scroll-button-right-svg" src={scrollArrowIcon} />
                         </button>
                     )}
                 </div>
