72d89fb3dd3b091382c31faaa39bccf3f9a85241
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-17 21:52:58 -0600

    update footer links to open in a new page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,13 +12,13 @@ export default function Footer() {
                 <img className="footer__logo-img" src={JourneyNookLogo} />
             </Link>
             <div className="footer__social">
-                <a className="footer__social-link" href="mailto:blikebahar76@gmail.com">
+                <a className="footer__social-link" href="mailto:blikebahar76@gmail.com" target="_blank">
                     <img className="footer__social-link-img" src={emailIcon} />
                 </a>
-                <a className="footer__social-link" href="https://github.com/b-like-bahar">
+                <a className="footer__social-link" href="https://github.com/b-like-bahar" target="_blank">
                     <img className="footer__social-link-img" src={githubIcon} />
                 </a>
-                <a className="footer__social-link" href="http://www.linkedin.com/in/bahareh-hamzeh">
+                <a className="footer__social-link" href="http://www.linkedin.com/in/bahareh-hamzeh" target="_blank">
                     <img className="footer__social-link-img" src={linkedinIcon} />
                 </a>
             </div>
