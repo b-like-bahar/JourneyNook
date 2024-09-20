b2567050338521cd80ae96fb292672d665163a66
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-20 08:47:14 -0600

    setup initial Express server with static file serving and CORS

---
+++ server(BackEnd)/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+const{ PORT, CORS_ORIGIN } = process.env;
+
+app.use(cors({ origin: CORS_ORIGIN }));
+app.use(express.json()); 
+app.use('/images', express.static("public/images"));
+
+app.use('/', (_req, res) => {
+    res.send("Hello API routes!!!");
+});
+
+app.use((err, _req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error'});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
