c4acf1526859ffac5e4dface99fcd89773f1b95d
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-21 07:11:15 -0600

    add a new route to app.jsx to handle route after clicking on each attraction

--- client(FrontEnd)/src/App.jsx
+++ client(FrontEnd)/src/App.jsx
@@ -1,28 +1,30 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Header from "./components/Header/Header.jsx";
 import Footer from './components/Footer/Footer.jsx';
-import HomePage from "./pages/HomePage/HomePage.jsx";
+import HomePage from './pages/HomePage/HomePage.jsx';
 import CityDetailPage from "./pages/CityDetailPage/CityDetailPage.jsx";
 import ItineraryPage from "./pages/ItineraryPage/ItineraryPage.jsx";
 import LoginPage from "./pages/LoginPage/LoginPage.jsx";
+
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-    <Header />
+      <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/city/:id" element={<CityDetailPage />} />
+          <Route path="/city/:cityId" element={<CityDetailPage />} />
+          <Route path="/city/:cityId/attractions/:attractionId" element={<CityDetailPage />} />
           <Route path="/itinerary" element={<ItineraryPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
