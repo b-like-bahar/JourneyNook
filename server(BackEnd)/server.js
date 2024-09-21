1eb5906fbd9bdfc69d73a60822929bf32b8ad8f3
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-20 20:37:56 -0600

    integrate cities router into server.js

--- server(BackEnd)/server.js
+++ server(BackEnd)/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import citiesRouter from "./routes/cities.js";
 
 const app = express();
 const{ PORT, CORS_ORIGIN } = process.env;
@@ -8,6 +9,8 @@ const{ PORT, CORS_ORIGIN } = process.env;
 app.use(cors({ origin: CORS_ORIGIN }));
 app.use(express.json()); 
 app.use('/images', express.static("public/images"));
+
+app.use('/cities', citiesRouter);
 
 app.use('/', (_req, res) => {
     res.send("Hello API routes!!!");
