944e9741edca4e18025725434cf0a492df4095f2
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-22 13:15:53 -0600

    add initial html structure to the itinerary page

--- client(FrontEnd)/src/pages/ItineraryPage/ItineraryPage.jsx
+++ client(FrontEnd)/src/pages/ItineraryPage/ItineraryPage.jsx
@@ -1,9 +1,26 @@
-import "./ItineraryPage.scss"
+import './ItineraryPage.scss';
+import { useNavigate, useLocation, useParams } from 'react-router-dom';
 
 function ItineraryPage() {
+    const { cityId } = useParams();
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const { itinerary, cityName } = location.state || {};
+
+    if (!itinerary) {
+        navigate(`/city/${cityId}`);
+        return null;
+    }
+
     return (
-        <div>ItineraryPage</div>
-    )
+        <div className="itinerary">
+            <h1 className="itinerary__title">Your Itinerary for {cityName}</h1>
+            <div className="itinerary__content">
+                <p>{itinerary}</p>
+            </div>
+        </div>
+    );
 }
 
-export default ItineraryPage
+export default ItineraryPage;
