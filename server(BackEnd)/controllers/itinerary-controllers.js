b4c670756d46e381087300e0ec10d1b0b7a4b592
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-23 00:52:43 -0600

    delete not needed parts for restucturing generated data in itinerary controllerfile at BE

--- server(BackEnd)/controllers/itinerary-controllers.js
+++ server(BackEnd)/controllers/itinerary-controllers.js
@@ -2,14 +2,13 @@ import axios from "axios";
 import { createTripPrompt } from "../middleware/middleware.js";
 
 const generateItinerary = async (req, res) => {
-
   const { days, budget, tripType, cityName } = req.body;
 
   if (!days || !budget || !tripType || !cityName) {
     return res.status(400).json({ error: 'All fields are required.' });
   }
 
-if (!process.env.OPENAI_API_KEY) {
+  if (!process.env.OPENAI_API_KEY) {
     return res.status(500).json({ error: 'OpenAI API key is missing' });
   }
 
@@ -27,44 +26,27 @@ if (!process.env.OPENAI_API_KEY) {
       content: content,
     }
   ];
-  let generatedItinerary = "";
-  let finishReason = null;
-  let continuationAttempts = 0;
-  const maxContinuationAttempts = 3;
 
   try {
-    while (finishReason !== "stop" && continuationAttempts <= maxContinuationAttempts) {
-      const response = await axios.post(
-        "https://api.openai.com/v1/chat/completions",
-        {
-          model: "gpt-3.5-turbo",
-          messages: messages,
-          max_tokens: maxTokens,
-          temperature: 0.3,
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo",
+        messages: messages,
+        max_tokens: maxTokens,
+        temperature: 0.2,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
         },
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-          },
-        }
-      );
-      const newContent = response.data.choices[0].message.content;
-      finishReason = response.data.choices[0].finish_reason;
-
-      generatedItinerary += newContent;
-      if (finishReason === "length") {
-        messages.push({
-          role: "assistant",
-          content: "Please continue the itinerary without repeating the previous steps."
-        });
-        continuationAttempts++;
-        maxTokens = 1000;
-      } else if (finishReason !== "length" && finishReason !== "stop") {
-        break;
       }
-    }
+    );
 
-    res.status(200).json({ itinerary: generatedItinerary });
+    const generatedItinerary = response.data.choices[0].message.content;
+
+    res.status(200).send(generatedItinerary);
+
   } catch (error) {
     console.error(
       "Error generating itinerary:",
@@ -74,6 +56,8 @@ if (!process.env.OPENAI_API_KEY) {
   }
 };
 
-export { 
-  generateItinerary 
-}
+export {
+  generateItinerary
+};
+
+
