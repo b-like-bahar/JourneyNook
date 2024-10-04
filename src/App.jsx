283ca1d86ba2b3b96a1e3f755036466eac2cde68
Bahar Hamzeh <blikebahar76@gmail.com>
2024-10-03 23:16:29 -0600

    delete server folder and add files in the client foder to root directory

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.scss';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import Header from "./components/Header/Header.jsx";
+import Footer from './components/Footer/Footer.jsx';
+import HomePage from './pages/HomePage/HomePage.jsx';
+import CityDetailPage from "./pages/CityDetailPage/CityDetailPage.jsx";
+import ItineraryPage from "./pages/ItineraryPage/ItineraryPage.jsx";
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/city/:cityId" element={<CityDetailPage />} />
+          <Route path="/city/:cityId/attractions/:attractionId" element={<CityDetailPage />} />
+          <Route path="/city/:cityId/itinerary" element={<ItineraryPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
